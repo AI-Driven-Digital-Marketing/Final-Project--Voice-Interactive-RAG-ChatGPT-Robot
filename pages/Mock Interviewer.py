7605b478ebd00ac45e17bf51701148934c830f55
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-04-18 18:47:30 -0400

    Update Mock Interviewer.py

--- pages/Mock Interviewer.py
+++ pages/Mock Interviewer.py
@@ -14,12 +14,12 @@ if audio_bytes:
     st.audio(audio_bytes, format="audio/wav")
 
 
-# sample_rate = 44100  # 44100 samples per second
-# seconds = 2  # Note duration of 2 seconds
-# frequency_la = 440  # Our played note will be 440 Hz
-# # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
-# t = np.linspace(0, seconds, seconds * sample_rate, False)
-# # Generate a 440 Hz sine wave
-# note_la = np.sin(frequency_la * t * 2 * np.pi)
+sample_rate = 44100  # 44100 samples per second
+seconds = 2  # Note duration of 2 seconds
+frequency_la = 440  # Our played note will be 440 Hz
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+t = np.linspace(0, seconds, seconds * sample_rate, False)
+# Generate a 440 Hz sine wave
+note_la = np.sin(frequency_la * t * 2 * np.pi)
 
-# st.audio(note_la, sample_rate=sample_rate)
+st.audio(note_la, sample_rate=sample_rate)
