dd057ec7e753e8d015ec245416433390a1d35254
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-04-18 18:46:30 -0400

    Update Mock Interviewer.py

--- pages/Mock Interviewer.py
+++ pages/Mock Interviewer.py
@@ -2,7 +2,13 @@ import streamlit as st
 import numpy as np
 from audio_recorder_streamlit import audio_recorder
 
-audio_bytes = audio_recorder()
+audio_bytes = audio_recorder(
+    text="Record your Questions",
+    recording_color="#e8b62c",
+    neutral_color="#6aa36f",
+    icon_name="user",
+    icon_size="6x",
+)
 
 if audio_bytes:
     st.audio(audio_bytes, format="audio/wav")
