a672d9fa1fcf474f1800646f74a1b6ccc7f3e826
Rayden X <xu.rund@northeastern.edu>
2023-04-18 10:55:10 -0400

    2

--- pages/Mock Interviewer.py
+++ pages/Mock Interviewer.py
@@ -1,2 +1,16 @@
 import streamlit as st
+from streamlit_audio_recorder import st_audio_recorder
+
 st.write('## Welcome :)')
+
+
+audio_file = st_audio_recorder(start_recording=False, 
+                               stop_recording=False, 
+                               recording_time=5, 
+                               sample_rate=44100, 
+                               audio_type='wav')
+
+if audio_file:
+    st.audio(audio_file)
+
+
