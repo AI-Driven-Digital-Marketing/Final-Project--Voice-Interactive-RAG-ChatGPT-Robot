4f7a17e80469affcc32f5fbe5426d2b53421992f
Rayden X <xu.rund@northeastern.edu>
2023-04-18 19:56:32 -0400

    3

--- pages/Mock Interviewer.py
+++ pages/Mock Interviewer.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import numpy as np
 from st_custom_components import st_audiorec
+import whisper
+
+
 # from audiorecorder import audiorecorder
 # audio = audiorecorder("Click to record", "Stop Recording...")
 
@@ -11,13 +14,39 @@ from st_custom_components import st_audiorec
 #     # To save audio to a file:
 #     wav_file = open("audio.mp3", "wb")
 #     wav_file.write(audio.tobytes())
-st.title("Audio Recorder")
-st.write("Click the button below to record your voice")
-wav_audio_data = st_audiorec()
+# input GUI for user
+_,col1,_ = st.columns([1,6,1])
+_,col2,_ = st.columns([1,6,1])
+col1, col2 = st.columns(2,gap = "medium")
 
-if wav_audio_data is not None:
-    # display audio data as received on the backend
-    st.audio(wav_audio_data, format='audio/wav')
+with col1:
+    st.title("Audio Recorder")
+    st.write("Click the button below to record your voice")
+    audio_data = st_audiorec()
+
+    if audio_data is not None:
+        # display audio data as received on the backend
+        st.audio(audio_data, format='audio/wav')
+        # INFO: by calling the function an instance of the audio recorder is created
+        # INFO: once a recording is completed, audio data will be saved to wav_audio_data
+with col2:
+# wav_audio_data
+model = whisper.load_model("base")
+def transcribe(audio):
     
-# INFO: by calling the function an instance of the audio recorder is created
-# INFO: once a recording is completed, audio data will be saved to wav_audio_data
+    #time.sleep(3)
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio(audio_data)
+    audio = whisper.pad_or_trim(audio_data)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio_data).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions()
+    result = whisper.decode(model, mel, options)
+    return result.text
