47af1e8bbf84bcdcbeaf1d036cc58a779d579f7f
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-04-18 18:54:29 -0400

    Update Mock Interviewer.py

--- pages/Mock Interviewer.py
+++ pages/Mock Interviewer.py
@@ -1,17 +1,27 @@
 import streamlit as st
 import numpy as np
-from audio_recorder_streamlit import audio_recorder
+from audiorecorder import audiorecorder
 
-audio_bytes = audio_recorder(
-    text="Record your Questions",
-    recording_color="#e8b62c",
-    neutral_color="#6aa36f",
-    icon_name="user",
-    icon_size="6x",
-)
+# audio_bytes = audio_recorder(
+#     text="Record your Questions",
+#     recording_color="#e8b62c",
+#     neutral_color="#6aa36f",
+#     icon_name="user",
+#     icon_size="6x",
+# )
 
-if audio_bytes:
-    st.audio(audio_bytes, format="audio/wav",sample_rate=44100)
+# if audio_bytes:
+#     st.audio(audio_bytes, format="audio/wav",sample_rate=44100)
 
 
 
+st.title("Audio Recorder")
+audio = audiorecorder("Click to record", "Recording...")
+
+if len(audio) > 0:
+    # To play audio in frontend:
+    st.audio(audio.tobytes())
+    
+    # To save audio to a file:
+    wav_file = open("audio.mp3", "wb")
+    wav_file.write(audio.tobytes())
