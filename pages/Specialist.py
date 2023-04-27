01837544dba1b8205b5992280ed3d49877bc1017
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-04-27 12:35:39 -0400

    Update Specialist.py

--- pages/Specialist.py
+++ pages/Specialist.py
@@ -7,7 +7,7 @@ import openai
 import pinecone
 import os 
 import pymysql
-# from langchain.llms import OpenAI
+from langchain.llms import OpenAI
 # from langchain.chains.question_answering import load_qa_chain
 # from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
 
