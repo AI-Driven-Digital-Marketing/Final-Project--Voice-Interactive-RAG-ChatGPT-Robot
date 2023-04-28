a6a9449f4e13056ee89286ed9c2542a55bd37a14
DDuan-zw <zwduan@outlook.com>
2023-04-28 16:30:20 -0400

    Update Specialist.py

--- pages/Specialist.py
+++ pages/Specialist.py
@@ -58,9 +58,90 @@ def initialize():
 
     return docsearch, chain
 
+@st.cache_resource
+def initialize_CRM():
+    openai.api_key = st.secrets['openai_key']
+    index_name = 'openai-youtube-transcriptions'
+
+    # initialize connection to pinecone (get API key at app.pinecone.io)
+    pinecone.init(
+        api_key=st.secrets['pinecone_key'],
+        environment=st.secrets['pinecone_env']  # may be different, check at app.pinecone.io
+    )
+
+    # check if index already exists (it shouldn't if this is first time)
+    if index_name not in pinecone.list_indexes():
+        # if does not exist, create index
+        pinecone.create_index(
+            index_name,
+            dimension=1536,
+            metric='cosine',
+            metadata_config={'indexed': ['title']}
+        )
+    # connect to index
+    index = pinecone.Index(index_name)
+    return index
+index = initialize_CRM()
+
+def complete(prompt):
+    # query text-davinci-003
+    res = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        temperature=0,
+        max_tokens=400,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+    return res['choices'][0]['text'].strip()
 
 
-#
+limit = 4000
+embed_model = "text-embedding-ada-002"
+def retrieve(query):
+    res = openai.Embedding.create(
+        input=[query],
+        engine=embed_model
+    )
+
+    # retrieve from Pinecone
+    xq = res['data'][0]['embedding']
+
+    # get relevant contexts
+    res = index.query(xq, top_k=3, include_metadata=True)
+    contexts = [
+        x['metadata']['text'] for x in res['matches']
+    ]
+
+    # build our prompt with the retrieved contexts included
+    prompt_start = (
+        "Answer the question based on the context below. If not related, ignore it.\n\n"+
+        "Context:\n"
+    )
+    prompt_end = (
+        f"\n\nQuestion: {query}\nAnswer:"
+    )
+    # append contexts until hitting limit
+    for i in range(1, len(contexts)):
+        if len("\n\n---\n\n".join(contexts[:i])) >= limit:
+            prompt = (
+                prompt_start +
+                "\n\n---\n\n".join(contexts[:i-1]) +
+                prompt_end
+            )
+            break
+        elif i == len(contexts)-1:
+            prompt = (
+                prompt_start +
+                "\n\n---\n\n".join(contexts) +
+                prompt_end
+            )
+    return prompt
+
+
+
 with tab1: 
     docsearch, chain  = initialize()
     form = st.form(key='myform1')
@@ -79,10 +160,22 @@ if submit:
 
 with tab2:
     st.write('Hubspot')
-
+    form = st.form(key='myform2')
+    query2 = form.text_input( "Enter some text 👇",
+        placeholder="Write your prompt here...",
+    )
+    submit2 = form.form_submit_button('Submit')
+if submit2:
+    # get context, additional info from pinecone
+    query_with_contexts = retrieve(query2)
+    # call openai API
+    output = complete(query_with_contexts)
+    with st.expander("See contexts prompt from the RAG"):
+        st.write(query_with_contexts)
+    st.write(output)
 
 with tab3: 
-    form = st.form(key='myform')
+    form = st.form(key='myform3')
     query = form.text_input( "Enter some text 👇",
         placeholder="Write your prompt here...",
     )
