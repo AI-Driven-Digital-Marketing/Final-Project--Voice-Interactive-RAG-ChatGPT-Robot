bfb64b5704c450f9bfe7c05bcd42b4c48065e6ab
Rayden X <xu.rund@northeastern.edu>
2023-04-28 15:14:17 -0400

    10

--- pages/Specialist.py
+++ pages/Specialist.py
@@ -106,8 +106,8 @@ if submit:
     db_uri = "mysql+pymysql:" + SQL_KEY
     db = SQLDatabase.from_uri(db_uri)
     llm = OpenAI(temperature=0, openai_api_key= OPENAI_API_KEY)
-    db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True) 
+    db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True,return_intermediate_steps=True) 
     result = db_chain.run(query)
-    st.markdown(result)
+    st.write(result)
 
 
