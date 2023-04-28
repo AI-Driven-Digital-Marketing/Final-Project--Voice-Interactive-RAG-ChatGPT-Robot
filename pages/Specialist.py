fccd6d6cd0dd33670852ad3f16658d0080c88d09
Smart Husky <64514218+DDuan-zw@users.noreply.github.com>
2023-04-27 23:13:35 -0400

    updates langchain

--- pages/Specialist.py
+++ pages/Specialist.py
@@ -6,13 +6,13 @@ import wave
 import openai
 import pinecone
 import os 
-#import pymysql
+import pymysql
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-# import langchain
-# from langchain.llms import OpenAI
-# from langchain.chains.question_answering import load_qa_chain
-# from langchain import SQLDatabase, SQLDatabaseChain
+from langchain.vectorstores import Chroma, Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+import langchain
+
 
 
 # input GUI for user
@@ -33,92 +33,30 @@ def save_wav(audio_data):
 
 def transcribe(audio):
     # model = initialize()
-    openai.api_key = st.secrets['openai_key']
+    openai.api_key = st.secrets['OPENAI_API_KEY']
     audio_file= open("audio_file.wav", "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return transcript
 
 @st.cache_resource
 def initialize():
-    openai.api_key = st.secrets['openai_key']
-    index_name = 'openai-embedding-data'
+    index_name = "langchain2"
 
     # initialize connection to pinecone (get API key at app.pinecone.io)
     pinecone.init(
-        api_key=st.secrets['pinecone_key'],
-        environment=st.secrets['pinecone_region']  # may be different, check at app.pinecone.io
+        api_key=st.secrets['PINECONE_API_KEY'],
+        environment=st.secrets['PINECONE_API_ENV']  # may be different, check at app.pinecone.io
     )
-
-    # check if index already exists (it shouldn't if this is first time)
-    if index_name not in pinecone.list_indexes():
-        # if does not exist, create index
-        pinecone.create_index(
-            index_name,
-            dimension=1536,
-            metric='cosine',
-            metadata_config={'indexed': ['title']}
-        )
+    OPENAI_API_KEY = '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'
+    embeddings = st.secrets['OPENAI_API_KEY']
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
     # connect to index
-    index = pinecone.Index(index_name)
-    return index
-index = initialize()
+    llm = OpenAI(temperature=0.5, openai_api_key=OPENAI_API_KEY )
+    chain = load_qa_chain(llm, chain_type="stuff")
 
-def complete(prompt):
-    # query text-davinci-003
-    res = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=prompt,
-        temperature=0,
-        max_tokens=400,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=None
-    )
-    return res['choices'][0]['text'].strip()
+    return docsearch, chain
+docsearch, chain  = initialize()
 
-
-limit = 4000
-embed_model = "text-embedding-ada-002"
-def retrieve(query):
-    res = openai.Embedding.create(
-        input=[query],
-        engine=embed_model
-    )
-
-    # retrieve from Pinecone
-    xq = res['data'][0]['embedding']
-
-    # get relevant contexts
-    res = index.query(xq, top_k=3, include_metadata=True)
-    contexts = [
-        x['metadata']['text'] for x in res['matches']
-    ]
-
-    # build our prompt with the retrieved contexts included
-    prompt_start = (
-        "Answer the question based on the context below.\n\n"+
-        "Context:\n"
-    )
-    prompt_end = (
-        f"\n\nQuestion: {query}\nAnswer:"
-    )
-    # append contexts until hitting limit
-    for i in range(1, len(contexts)):
-        if len("\n\n---\n\n".join(contexts[:i])) >= limit:
-            prompt = (
-                prompt_start +
-                "\n\n---\n\n".join(contexts[:i-1]) +
-                prompt_end
-            )
-            break
-        elif i == len(contexts)-1:
-            prompt = (
-                prompt_start +
-                "\n\n---\n\n".join(contexts) +
-                prompt_end
-            )
-    return prompt
 
 # with col1:
 #     st.title("Audio Recorder")
@@ -150,9 +88,9 @@ with col3:
     submit = form.form_submit_button('Submit')
 if submit:
     # get context, additional info from pinecone
-    query_with_contexts = retrieve(query)
+    docs = docsearch.similarity_search(query= query, include_metadata=True)
     # call openai API
-    output = complete(query_with_contexts)
-    with st.expander("See contexts prompt from the RAG"):
-        st.write(query_with_contexts)
-    st.write(output)
+    result = chain.run(input_documents=docs, question=query)
+    with st.expander("See searched docs here."):
+        st.write(docs)
+    st.write(result)
