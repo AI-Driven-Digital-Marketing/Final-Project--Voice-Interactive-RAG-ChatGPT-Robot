07b0e9410213486c2af90f99232c2a38eac8dec8
Smart Husky <64514218+DDuan-zw@users.noreply.github.com>
2023-04-28 21:38:02 -0400

    Update Specialist.py

--- pages/Specialist.py
+++ pages/Specialist.py
@@ -165,12 +165,12 @@ with tab1:
 with tab2:
     st.write('Hubspot Company Knowledge Base (Internal-Public)')
     index = initialize_CRM()
-    #audio_data2 = audio_recorder(pause_threshold=4.0, icon_size = '2x')
+    audio_data1 = audio_recorder(pause_threshold=4.0, icon_size = '2x')
     default_input2= ''
-    if audio_data2 is not None:
+    if audio_data1 is not None:
         # display audio data as received on the backend
-        save_wav(audio_data2)
-        default_input2 = transcribe(audio_data2)['text']
+        save_wav(audio_data1)
+        default_input2 = transcribe(audio_data1)['text']
     form = st.form(key='myform2')
     query2 = form.text_input( "Ask how to use CRM 👇",
         placeholder="Write your prompt here...",
