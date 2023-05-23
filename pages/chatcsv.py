c1b5fa78e1218b5ea396d54a3781f1cd783e82ae
Rayden X <xu.rund@northeastern.edu>
2023-05-22 23:02:43 -0400

    1

--- pages/chatcsv.py
+++ pages/chatcsv.py
@@ -108,8 +108,8 @@ with tab1:
                     message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
 
 with tab2:
-    df2 = pd.DataFrame(eval(uploaded_file.read()))
-    st.write(df2)
+    # df2 = pd.DataFrame(eval(uploaded_file.read()))
+    st.write('df2')
 #     profile = profiling_transaction(df)
 #     st_profile_report(profile)
 #     st.download_button(
